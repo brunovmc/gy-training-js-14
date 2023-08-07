ebb4eea6076088049f62a0bd4201cc5e42ffa363
brunovmc <bruno.vidigal.coriolano@gmail.com>
2023-08-07 13:42:07 -0300

    feat: add two endpoints for daily and weekly report generation

--- src/services/dailyAggregation.service.js
+++ src/services/dailyAggregation.service.js
@@ -1,33 +1,36 @@
-const { convertToISODate } = require('../utils/dateConversor');
+const { parse, format } = require('date-fns');
 
 function calculateDailyAggregation() {
-  const dailyAggregation = {};
+    const dailyAggregation = {};
 
-  function onData(row) {
-    const isoDateString = convertToISODate(row.dateTime);
-    const value = Number(row.value.replace(',', '.'));
+    function onData(row) {
+        const customDateFormat = 'dd/MM/yyyy HH:mm:ss';
+        const dateObject = parse(row.dateTime, customDateFormat, new Date());
+        const isoDateString = format(dateObject, 'yyyy-MM-dd');
+        const value = Number(row.value.replace(',', '.'));
 
-    if (!isNaN(value)) {
-      if (dailyAggregation[isoDateString]) {
-        dailyAggregation[isoDateString].sum += value;
-        dailyAggregation[isoDateString].count++;
-      } else {
-        dailyAggregation[isoDateString] = { sum: value, count: 1 };
-      }
+        aggregateData(isoDateString, value);
     }
-  }
 
-  function onEnd() {
-    console.log("Daily Aggregation:");
-    for (const date in dailyAggregation) {
-      dailyAggregation[date].average = dailyAggregation[date].sum / dailyAggregation[date].count;
-      console.log(`${date} - Average Value: ${dailyAggregation[date].average.toFixed(2)}`);
+    function aggregateData(isoDateString, value) {
+        if (!isNaN(value)) {
+            dailyAggregation[isoDateString] = dailyAggregation[isoDateString] || { sum: 0, count: 0 };
+            dailyAggregation[isoDateString].sum += value;
+            dailyAggregation[isoDateString].count++;
+        }
     }
-  }
 
-  return { onData, onEnd };
+    function onEnd() {
+        console.log("Daily Aggregation:");
+        for (const date in dailyAggregation) {
+            dailyAggregation[date].average = dailyAggregation[date].sum / dailyAggregation[date].count;
+            console.log(`${date} - Average Value: ${dailyAggregation[date].average.toFixed(2)}`);
+        }
+    }
+
+    return { onData, onEnd };
 }
 
 module.exports = {
-  calculateDailyAggregation
+    calculateDailyAggregation
 };
--- src/services/weeklyAggregation.service.js
+++ src/services/weeklyAggregation.service.js
@@ -1,53 +1,56 @@
-const { parseDateTime } = require('../utils/dateConversor');
+const { parse, addWeeks } = require('date-fns');
 
 function calculateWeeklyAggregation() {
-  const weeklyData = {};
-  let currentWeekStart = null; 
+    const weeklyData = {};
+    let currentWeekStart = null;
 
-  function onData(row) {
-    const alarmThreshold = 1;
+    function aggregateWeeklyData(isoDateString, value) {
+        const alarmThreshold = 1;
 
-    const date = parseDateTime(row.dateTime);
+        weeklyData[isoDateString] = weeklyData[isoDateString] || { sum: 0, count: 0, alarmCount: 0 };
+        
+        weeklyData[isoDateString].sum += value;
+        weeklyData[isoDateString].count++;
 
-    if (currentWeekStart === null) {
-      currentWeekStart = new Date(date.getFullYear(), date.getMonth(), date.getDate(), 0, 0, 0);
+        if (value > alarmThreshold) {
+            weeklyData[isoDateString].alarmCount++;
+        }
     }
 
-    const nextWeekStart = new Date(currentWeekStart.getTime() + 7 * 24 * 60 * 60 * 1000);
+    function onData(row) {
+        const customDateFormat = 'dd/MM/yyyy HH:mm';
+        const date = parse(row.dateTime, customDateFormat, new Date());
 
-    if (date >= nextWeekStart) {
-      currentWeekStart = new Date(date.getFullYear(), date.getMonth(), date.getDate(), 0, 0, 0);
+        if (!currentWeekStart) {
+            currentWeekStart = new Date(date.getFullYear(), date.getMonth(), date.getDate(), 0, 0, 0);
+        }
+
+        const nextWeekStart = addWeeks(currentWeekStart, 1);
+
+        if (date >= nextWeekStart) {
+            currentWeekStart = new Date(date.getFullYear(), date.getMonth(), date.getDate(), 0, 0, 0);
+        }
+
+        const isoDateString = currentWeekStart.toISOString().split('T')[0];
+        const value = Number(row.value.replace(',', '.'));
+
+        if (!isNaN(value)) {
+            aggregateWeeklyData(isoDateString, value);
+        }
     }
 
-    const isoDateString = currentWeekStart.toISOString().split('T')[0];
-    const value = Number(row.value.replace(',', '.'));
-
-    if (!isNaN(value)) {
-      if (!weeklyData[isoDateString]) {
-        weeklyData[isoDateString] = { sum: 0, count: 0, alarmCount: 0 };
-      }
-
-      weeklyData[isoDateString].sum += value;
-      weeklyData[isoDateString].count++;
-
-      if (value > alarmThreshold) {
-        weeklyData[isoDateString].alarmCount++;
-      }
+    function onEnd() {
+        console.log("Weekly Aggregation Report:");
+        for (const weekStartDate in weeklyData) {
+            const { sum, count, alarmCount } = weeklyData[weekStartDate];
+            const weeklyAverage = sum / count;
+            console.log(`${weekStartDate} - Weekly Average: ${weeklyAverage.toFixed(2)} - Alarms Triggered: ${alarmCount}`);
+        }
     }
-  }
 
-  function onEnd() {
-    console.log("Weekly Aggregation Report:");
-    for (const weekStartDate in weeklyData) {
-      const { sum, count, alarmCount } = weeklyData[weekStartDate];
-      const weeklyAverage = sum / count;
-      console.log(`${weekStartDate} - Weekly Average: ${weeklyAverage.toFixed(2)} - Alarms Triggered: ${alarmCount}`);
-    }
-  }
-
-  return { onData, onEnd };
+    return { onData, onEnd };
 }
 
 module.exports = {
-  calculateWeeklyAggregation
+    calculateWeeklyAggregation
 };
